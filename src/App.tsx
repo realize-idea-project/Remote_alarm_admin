ed162592a62112a3a3d6915cdc48c8a4f1832bb1
han <han@zamface.co.kr>
2023-06-12 14:38:53 +0900

    connect realtime

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,26 @@
 import React from "react";
 import "./App.css";
+import { useFB } from "./useFB";
 
 function App() {
-  return <div className="App">ㅗㅑ</div>;
+  const { getData, setData } = useFB();
+
+  const handleClick = async () => {
+    const date = new Date().getDate();
+    console.log(await getData(date));
+  };
+
+  const handleSet = () => {
+    const date = new Date().getDate();
+    setData(date, "14:33,16:36,18:00");
+  };
+
+  return (
+    <div className="App">
+      <button onClick={handleClick}>get</button>
+      <button onClick={handleSet}>set</button>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/useFB.ts
@@ -0,0 +1,31 @@
+import { useEffect } from "react";
+import { db } from "./firebase";
+
+export const useFB = () => {
+  const id = 1;
+  const realTime = db.ref(`company/company_${id}`);
+
+  useEffect(() => {
+    realTime.on("value", (snapshot) => {
+      const data = snapshot.val();
+      console.log("data!!", data);
+    });
+  }, []);
+
+  const getData = async (date: number) => {
+    const snapshot = await realTime.get();
+
+    if (snapshot.exists()) {
+      return snapshot.val();
+    }
+  };
+
+  const setData = async (date: number, time: string) => {
+    await realTime.set({ [date]: time });
+  };
+
+  return {
+    getData,
+    setData,
+  };
+};
